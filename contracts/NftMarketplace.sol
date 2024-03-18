24b319e691b6502838d0d06360e3bc7c39dbf2f3
anhtoan260497 <anhtoan260497@gmail.com>
2024-03-18 16:24:46 +0700

    finish

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -2,13 +2,16 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/interfaces/IERC721.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 error NftMarketplace__InsufficentPrice();
 error NftMarketplace__NotApproved();
 error NftMarketplace__AlreadyListed(address nftAddress, uint256 tokenId);
-error NftMarketplace__NotNftOwnerForListing(address nftAddress, uint256 tokenId);
+error NftMarketplace__NotNftOwnerForListing();
+error NftMarketplace__NotListed(address nftAddress, uint256 tokenId);
+error NftMarketplace__NotEnoughBalanceToBuy(address nftAddress, uint256 tokenId, uint256 price);
 
-contract NftMarketplace {
+contract NftMarketplace is ReentrancyGuard {
     struct Listing {
         uint256 price;
         address seller;
@@ -21,7 +24,15 @@ contract NftMarketplace {
         uint256 price
     );
 
+    event ItemBought(
+        address indexed buyer,
+        address indexed NftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+
     mapping(address => mapping(uint256 => Listing)) private s_listing; // s_listing : {address : { tokenid : { price, seller }}}
+    mapping(address => uint256) private s_proceeds;
 
     modifier NotListed(
         address nftAddress,
@@ -40,7 +51,13 @@ contract NftMarketplace {
     ) {
         IERC721 nft = IERC721(nftAddress);
         address owner = nft.ownerOf(tokenId);
-        if (owner != spender) revert NftMarketplace__NotNftOwnerForListing(nftAddress, tokenId);
+        if (owner != spender) revert NftMarketplace__NotNftOwnerForListing();
+        _;
+    }
+
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listing[nftAddress][tokenId];
+        if (listing.price <= 0) revert NftMarketplace__NotListed(nftAddress, tokenId);
         _;
     }
 
@@ -59,4 +76,22 @@ contract NftMarketplace {
         s_listing[nftAddress][tokenId] = Listing(price, msg.sender);
         emit ItemList(msg.sender, nftAddress, tokenId, price);
     }
+
+    function buyItem(
+        address nftAddress,
+        uint256 tokenId
+    ) external payable isListed(nftAddress, tokenId) nonReentrant {
+        Listing memory listedItem = s_listing[nftAddress][tokenId];
+        if (msg.value > listedItem.price)
+            revert NftMarketplace__NotEnoughBalanceToBuy(nftAddress, tokenId, msg.value);
+
+        s_proceeds[listedItem.seller] = s_proceeds[listedItem.seller] + msg.value;
+        delete (s_listing[nftAddress][tokenId]);
+        IERC721 nft = IERC721(nftAddress);
+
+        nft.safeTransferFrom(listedItem.seller, msg.sender, tokenId);
+        emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
+    }
+
+    
 }
